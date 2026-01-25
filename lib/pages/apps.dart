f0531c3d43fb4af8fa7010274682eaeed2bc905d
Omer I.S. <137101815+omeritzics@users.noreply.github.com>
2026-01-25 21:58:45 +0200

    Update lib/pages/apps.dart
    Co-authored-by: qodo-free-for-open-source-projects[bot] <189517486+qodo-free-for-open-source-projects[bot]@users.noreply.github.com>

--- lib/pages/apps.dart
+++ lib/pages/apps.dart
@@ -171,11 +171,11 @@ class AppsPageState extends State<AppsPage> {
   var sourceProvider = SourceProvider();
 
   @override
-  // _searchController.addListener(() {
-  //   setState(() {
-  //     _searchQuery = _searchController.text;
-  //   });
-  // });
+  void initState() {
+    super.initState();
+    _searchController = TextEditingController();
+    _searchController.addListener(() {
+      setState(() {
         _searchQuery = _searchController.text;
       });
     });
